03b483fad9a3fc61a18859aa63bfd245c1ff9937
shivamdargan <darganshivam2001@gmail.com>
2022-03-10 02:06:23 +0530

    Added Dynamic Landing Page

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -1,20 +1,64 @@
 import React from 'react'
 import '../../assets/css/stats.css';
-
+import URL from "../../URL";
+import { useState,useEffect } from 'react';
 const Stats = () => {
+  const [statsData,setStatsData] = useState();
+  const [dailyCases,setDailyCases] = useState();
+  const getStats = () => {
+    fetch(`${URL}/stats/cases`,  {credentials: "include"})
+    .then(async response => {
+        if(response.ok){
+            response.json().then(data => {
+             
+               setStatsData(data)
+            });
+         }
+        else{
+            throw response.json();
+        }
+      })
+      .catch(async (error) => {
+       
+        const errorMessage = await error;
+        console.log(errorMessage)
+      })
+}
+const getFiledCases = () => {
+  fetch(`${URL}/dailyFilled/cases`,  {credentials: "include"})
+  .then(async response => {
+      if(response.ok){
+          response.json().then(data => {
+             setDailyCases(data)
+          });
+       }
+      else{
+          throw response.json();
+      }
+    })
+    .catch(async (error) => {
+     
+      const errorMessage = await error;
+      console.log(errorMessage)
+    })
+}
+useEffect(() =>{
+  getStats();
+  getFiledCases();
+ }, [])
   return (
     <div className='stats-section'>
         <div className='stats-card'>
-            <p className='stats-num'>5.73 M</p>
-            <p className='stats-name'>HC PENDING CASES</p>
+            <p className='stats-num'>{statsData !== undefined ? statsData.openCases : "Loading.."}</p>
+            <p className='stats-name'>OPEN CASES</p>
         </div>
         <div className='stats-card'>
-            <p className='stats-num'>43.06 M</p>
-            <p className='stats-name'>DC PENDING CASES</p>
+            <p className='stats-num'>{statsData !== undefined ? statsData.closedCases : "Loading.."}</p>
+            <p className='stats-name'>CLOSED CASES</p>
         </div>
         <div className='stats-card'>
-            <p className='stats-num'>1.58 M</p>
-            <p className='stats-name'>HC CASES LISTED TODAY</p>
+            <p className='stats-num'>{dailyCases !== undefined ? dailyCases.noOfCases : "Loading.."}</p>
+            <p className='stats-name'>DAILY CASES LISTED </p>
         </div>
     </div>
   )
