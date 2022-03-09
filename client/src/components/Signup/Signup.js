c58786d48ec6228f6a254e35598183171b502f52
shivamdargan <darganshivam2001@gmail.com>
2022-03-10 03:55:47 +0530

    SIGNUP LAWYER INTEGRATED

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,10 +1,75 @@
 import React from 'react'
 import '../../assets/css/signup.css';
+import swal from "sweetalert"
 import signupimg from '../../assets/media/signup.png';
-
+import { useState } from 'react';
+import { Redirect } from 'react-router'
+import URL from '../../URL';
 const Signup = () => {
+    const [redirect,setRedirect] = useState(null);
+    const [userEnteredData, setuserEnteredData] = useState({
+        name: "",
+        email: "",
+        uid: "",
+        password: "",
+        location:"",
+        type:"judge"
+    })
+
+    const handleInput = (event) =>
+    {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setuserEnteredData({...userEnteredData, [name]:value })
+
+    }
+
+    const submitHandler = (event) =>
+    {
+        event.preventDefault()
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body:JSON.stringify(userEnteredData),  
+            credentials: "include"
+            };
+            fetch(`http://localhost:5000/register`, requestOptions )
+            .then(async response => {
+                response.json().then(data =>  {
+                  
+                    if(response.ok){
+                      
+                        swal({
+                          title: "Success!",
+                          text: "User Created Successfully",
+                          icon: "success",
+                        });
+                        setRedirect(<Redirect to="/login"/>)
+
+                     }
+                    else{
+                      swal({
+                        title: "Failed!",
+                        text: data._message === undefined ? "Already registered With Email Address":data._message,
+                        icon: "error",
+                      });
+                        //throw response.json();
+                    }
+                  });
+                
+              })
+              .catch(async (error) => {
+                const errorMessage = await error;
+                console.log(errorMessage)
+                
+              })
+    }
+
+
   return (
     <div className='signup'>
+        {redirect}
         <div className='signup-left'>
             <div className='signup-head'>
                 <h1>E-Adaalat Management</h1>
@@ -20,16 +85,17 @@ const Signup = () => {
                         <a href="./signup" style={{color:"#C89A69", textDecoration: "underline"}}>Signup as Judge</a>
                         <a href='./signuplawyer'>Signup as Lawyer</a>
                     </div>
-                    <div className='form'>
-                        <input className='forminput' type="text" name="name" placeholder='Name'></input>
-                        <input className='forminput' type="text" name="email" placeholder='Email'></input>
-                        <input className='forminput' type="password" name="password" placeholder='Password'></input>
-                        <input className='forminput' type="text" name="Uid" placeholder='UID'></input>
-                        <input className='forminput' type="text" name="location" placeholder='Court Location'></input>
-                    </div>
-                    <button className='signupbtn'>
-                        Login
-                    </button>
+                    <form className='form' id ="judgeForm">
+                        <input className='forminput' type="text" name="name" placeholder='Name' value = {userEnteredData.name} onChange = {handleInput}></input>
+                        <input className='forminput' type="text" name="email" placeholder='Email'  value = {userEnteredData.email} onChange = {handleInput}></input>
+                        <input className='forminput' type="password" name="password" placeholder='Password' value = {userEnteredData.password}  onChange = {handleInput}></input>
+                        <input className='forminput' type="text" name="uid" placeholder='Unique Identification Number' value = {userEnteredData.uid}  onChange = {handleInput}></input>
+                        <input className='forminput' type="text" name="location" placeholder='Court Name' value = {userEnteredData.location} onChange = {handleInput}></input>
+                        <a onClick={submitHandler}>
+                        <input className="signupbtn" form = "judgeForm" type="submit" value="Sign Up" />
+                    </a>
+                    </form>
+                   
                     <p>Already have an account? <a className='signup-a'>Login</a></p>
                 </div>
             </div>
