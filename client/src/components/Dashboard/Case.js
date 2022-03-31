4a64a47010063a2ce7f63d8820af5a086e8d7af8
shivamdargan <darganshivam2001@gmail.com>
2022-03-31 23:48:06 +0530

    Static Changes

--- client/src/components/Dashboard/Case.js
+++ client/src/components/Dashboard/Case.js
@@ -7,35 +7,34 @@ const Case=() =>{
             <SideNav/>
             <div className="right"> 
                 <div className="top">
-                    <h1>The State of Punjab vs Govind Singh</h1>
+                    <h1>Aman vs Manoj Kumar</h1>
                 </div>
                 <div className="one">
                     <h3>Description</h3>
-                    <p>Lörem ipsum lir lockdown regt spede, ninade märad. Studsmattesjuka höst monosk bioras. Ament räsk. Epigen bosede näkast vengen provis osk. Bevis medelsiv i prede, multidorat. Rer multist krossade inte poktigt. 
-Sasat pir anteng prest huruvida odode. Apotropi sens. Ynade sys i tingar tisöktig kafyledes. Sijyvis pseudosm tining lökanar polyn. Fonoception dis disabelt, pygapp, milogi kvasitopi. Kossade misunar inte suvis, synvision, geosöjyr ultranera.
+                    <p> Import or export of counterfeit coin.—Whoever imports into 1[India], or exports therefrom, any counterfeit ... coin, knowing or having reason to believe that the same is counterfeit, shall be punished with imprisonment of either description
 </p>
                 </div>
                 <div className="one two">
                     <div className="l">
                         <div>
-                            <h3>CRN NO.:</h3><h6>HFNE420691232022</h6>
+                            <h3>CNR NO:</h3><h6>HFNE420691232022</h6>
                         </div>
                         <div>
-                            <h3>Clause NO.:</h3><h6>403</h6>
+                            <h3>Clause NO:</h3><h6>403</h6>
                         </div>
                         <div>
-                            <h3>Court Location:</h3><h6>Punjab High Court</h6>
+                            <h3>Court Location:</h3><h6>Mumbai High Court</h6>
                         </div>
                     </div>
                     <div className="l r">
                         <div>
-                            <h3>Date of Filing:</h3><h6>12/04/19</h6>
+                            <h3>Date of Filing:</h3><h6>1/04/22</h6>
                         </div>
                         <div>
-                            <h3>Last Hearing Date:</h3><h6>02/02/22</h6>
+                            <h3>Last Hearing Date:</h3><h6>Not Heard Yet</h6>
                         </div>
                         <div>
-                            <h3>Next Hearing Date:</h3><h6>22/02/22</h6>
+                            <h3>Next Hearing Date:</h3><h6>Not Heard Yet</h6>
                         </div>
                     </div>
                 </div>
@@ -44,10 +43,10 @@ Sasat pir anteng prest huruvida odode. Apotropi sens. Ynade sys i tingar tisökt
                     <div className="two">
                         <div className="l">
                             <div>
-                                <h3>Name:</h3><h6>Govind Singh</h6>
+                                <h3>Name:</h3><h6>Manoj Kumar</h6>
                             </div>
                             <div>
-                                <h3>Phone NO.:</h3><h6>981xxxxxxx</h6>
+                                <h3>Phone NO:</h3><h6>9721387218</h6>
                             </div>
                         </div>
                         <div className="l r">
