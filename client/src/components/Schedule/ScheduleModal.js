f29f0c0d7a102abaf3540277d2f1b5693ac0c17e
shivamdargan <darganshivam2001@gmail.com>
2022-04-26 01:38:17 +0530

    Added Modal Correct

--- client/src/components/Schedule/ScheduleModal.js
+++ client/src/components/Schedule/ScheduleModal.js
@@ -5,39 +5,45 @@ import Button from 'react-bootstrap/Button'
 
 const ScheduleModal = (props) => {
   return (
-    // <div className='modal'>
-    //     <div className='modal-form'>
-    //     <form className='form'>
-    //         <label className='modal-head' for="birthdaytime">Choose Date & Time:</label>
-    //         <input type="datetime-local" id="birthdaytime" name="birthdaytime"/>
-    //         <input type="submit"/>
-    //     </form>
-    //     </div>
-    // </div>
-    <div>
-      <Modal
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-    </div> 
+    <>
+    {props.show ?  <div className='modal'>
+        <div className='modal-form'>
+        <form className='form'>
+            <label className='modal-head' for="birthdaytime">Choose Date & Time:</label>
+            <input type="datetime-local" id="birthdaytime" name="birthdaytime"/>
+            <input type="submit"/>
+            <a onClick={props.onHide}> hello</a>
+        </form>
+        </div>
+    </div> : null}
+    
+    
+    </>
+    // <div>
+    //   <Modal
+    //   {...props}
+    //   size="lg"
+    //   aria-labelledby="contained-modal-title-vcenter"
+    //   centered
+    // >
+    //   <Modal.Header closeButton>
+    //     <Modal.Title id="contained-modal-title-vcenter">
+    //       Modal heading
+    //     </Modal.Title>
+    //   </Modal.Header>
+    //   <Modal.Body>
+    //     <h4>Centered Modal</h4>
+    //     <p>
+    //       Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+    //       dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+    //       consectetur ac, vestibulum at eros.
+    //     </p>
+    //   </Modal.Body>
+    //   <Modal.Footer>
+    //     <Button onClick={props.onHide}>Close</Button>
+    //   </Modal.Footer>
+    // </Modal>
+    // </div> 
   )
 }
 
